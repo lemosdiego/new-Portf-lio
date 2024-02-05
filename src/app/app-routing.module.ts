4a4ae0fd1323dfac16af863dbad78f3c8194e5f4
diego.lemos <57995631+lemosdiego@users.noreply.github.com>
2024-02-05 16:13:05 -0300

    Navegação entre rotas Criadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartComponent } from './components/start/start.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: StartComponent, pathMatch:'full'},
+  {path: 'aboutme', component: AboutmeComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'contacts', component: ContactsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { IndexComponent } from './components/index/index.component';
+import { StartComponent } from './components/start/start.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    IndexComponent,
+    StartComponent,
+    AboutmeComponent,
+    ProjectsComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
