6a2443e14366a33229b6745499cd7e3131e3c6b8
diego.lemos <57995631+lemosdiego@users.noreply.github.com>
2024-02-05 21:28:17 -0300

    Melhorando o  Projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { StartComponent } from './components/start/start.component';
 import { AboutmeComponent } from './components/aboutme/aboutme.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactsComponent } from './components/contacts/contacts.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 const routes: Routes = [
   {path: '', component: StartComponent, pathMatch:'full'},
   {path: 'aboutme', component: AboutmeComponent},
   {path: 'projects', component: ProjectsComponent},
-  {path: 'contacts', component: ContactsComponent}
+  {path: 'contacts', component: ContactsComponent},
+  {path: 'skills', component: SkillsComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './shared/menu/menu.component';
-import { IndexComponent } from './components/index/index.component';
 import { StartComponent } from './components/start/start.component';
 import { AboutmeComponent } from './components/aboutme/aboutme.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactsComponent } from './components/contacts/contacts.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    IndexComponent,
     StartComponent,
     AboutmeComponent,
     ProjectsComponent,
-    ContactsComponent
+    ContactsComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
